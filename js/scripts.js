a8074f7c2b1ba1b05e9c5c86352782701fd723a2
Matt Luker <jamesmattluker@gmail.com>
2021-10-30 16:26:25 -0600

    rearange branching to pass seventh test

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,16 @@ function changeRangeArray(array) {
   const secondExceptionNumber = [2];
   const thirdExceptionNumber = [3]
   numberArray.forEach(function(element) {
-    if (element.includes(firstExceptionNumber)) {
+    if (element.includes(thirdExceptionNumber)) {
       let index = numberArray.indexOf(element);
-      numberArray.splice(index, 1, "Beep!");
+      numberArray.splice(index, 1, "Won't you be my neighbor?");
     } else if (element.includes(secondExceptionNumber)) {
       let index = numberArray.indexOf(element);
       numberArray.splice(index, 1, "Boop!");
-    } else if (element.includes(thirdExceptionNumber)) {
-      let index = numberArray.indexOf(element);
-      numberArray.splice(index, 1, "Won't you be my neighbor?");
-    } 
+    } else if (element.includes(firstExceptionNumber)) {
+        let index = numberArray.indexOf(element);
+        numberArray.splice(index, 1, "Beep!");
+    }    
   })
   return(numberArray);
 };
